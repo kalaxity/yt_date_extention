cd3cfca6289352212e01bec21bbfc872c8dc0702
kalaxity <insecticide.cleaner460@gmail.com>
2022-06-26 23:52:56 +0900

    フォーマット変更＆閲覧数も表示するように変更

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ if (document.URL.search("https://www.youtube.com/watch") > -1) {
 	let id_int = setInterval(() => {
 		if (document.querySelector("#owner-sub-count") != null){
 			clearInterval(id_int);
-			document.getElementById("owner-sub-count").innerText += " / " + document.querySelector("div#info-strings>yt-formatted-string").innerText;
+			document.getElementById("owner-sub-count").innerText += "\n" 
+				+ document.querySelector("div#info-strings>yt-formatted-string").innerText + "\n" 
+				+ document.querySelector("div#count>ytd-video-view-count-renderer>span.view-count").innerText;
 		}
 	}, 300);
 	console.log("投稿日時をチャンネル登録者数の部分に追記したよ")
